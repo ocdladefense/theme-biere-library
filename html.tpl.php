b2f25e2ab6d25246d4d9aa3dcf1e4e89597bf822
Ubuntu <ubuntu@ip-172-31-58-171.us-west-2.compute.internal>
2023-10-24 20:59:04 -0700

    Use GA debug mode when enabled.

--- html.tpl.php
+++ html.tpl.php
@@ -13,22 +13,23 @@
             function gtag() { dataLayer.push(arguments); }
             gtag('js', new Date());
             
-            gtag('config', '<?php print getGaProperyId(); ?>');
-            // gtag('config', '<?php print getGaProperyId(); ?>',{'debug_mode':true});
+            <?php if(isGaDebug()): ?>
+                gtag('config', '<?php print getGaProperyId(); ?>',{'debug_mode':true});
+            <?php else: ?>
+                gtag('config', '<?php print getGaProperyId(); ?>');
+            <?php endif; ?>
         </script>
+        <title><?= $title ?></title>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=yes" />
         <meta http-equiv="X-UA-Compatible" content="IE=Edge" />
-        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/<?= $themeUrl ?>/assets/images/favicon.ico" type="image/x-icon" />
         <link rel="shortcut icon" href="https://library.ocdla.org/<?= $themeUrl ?>/images/favicon.ico?foo=bar" type="image/x-icon" />
         <!-- <base href="/" /> -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Bitter:wght@100;200;300;400;500&family=Nanum+Pen+Script&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="node_modules/@ocdladefense/modal/src/css/loading.css" />
-        <link rel="stylesheet" href="node_modules/@ocdladefense/modal/src/css/modal.css" />
-        <link rel="stylesheet" href="node_modules/@ocdladefense/modal/src/css/inline-modal.css" />
         <link rel="stylesheet" href="<?= $themeUrl ?>/css/main.css" />
         <!-- <link rel="stylesheet" href="<?= $themeUrl ?>/css/food.css" /> -->
         <!-- <link rel="stylesheet" href="<?= $themeUrl ?>/css/drink.css" /> -->
@@ -47,28 +48,6 @@
         </div>
     </body>
 
-    <!-- 
-        <script type="module">
-
-            // https://medium.com/streak-developer-blog/the-complexities-of-implementing-inline-autocomplete-for-content-editables-e358c0ed504b
-            import {WebcEvents} from "./node_modules/@ocdladefense/webc-events/dist/WebcEvents.js";
-            import "./node_modules/@ocdladefense/html/html.js";
-            import domReady from "./node_modules/@ocdladefense/web/src/web.js";
-            // https://medium.com/streak-developer-blog/the-complexities-of-implementing-inline-autocomplete-for-content-editables-e358c0ed504b
-
-            // customElements.define("word-count", WordCount, { extends: "p" });
-            customElements.define("webc-events", WebcEvents);
-
-            window.init = init;
-            domReady(init);
-
-            async function init() {
-                console.log("init called.");
-            }
-        </script>
-
-        
-    -->
 
 
 </html>
